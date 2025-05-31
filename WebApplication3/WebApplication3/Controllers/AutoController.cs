9f4b381c3e846bf01dcfa3c1d59c581e07ec9ddf
marioaje <marioaje@gmail.com>
2025-05-30 19:33:31 -0600

    Aplicacion Web

---
+++ WebApplication3/WebApplication3/Controllers/AutoController.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class AutoController : Controller
+    {
+        #region propiedades
+
+        private static List<AutoModel> autoDatos = new List<AutoModel>();
+
+        #endregion
+
+        #region eventos
+        public IActionResult Index()
+        {
+            return View(autoDatos);
+        }
+
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        //Busca y encuentra el elemto y lo carga en el formulario
+        public IActionResult Eliminar(int id)
+        {
+            var _autoDatos = autoDatos.FirstOrDefault(x => x.Id == id);
+
+            if (_autoDatos == null) 
+            {
+                return NotFound();
+            }
+
+             return View(_autoDatos);
+
+        }
+
+        public IActionResult Editar(int id)
+        {
+            var _autoDatos = autoDatos.FirstOrDefault(x => x.Id == id);
+
+            if (_autoDatos == null)
+            {
+                return NotFound();
+            }
+
+            return View(_autoDatos);
+
+        }
+
+        [HttpPost, ActionName("EliminarRegistro")]
+        public IActionResult EliminarRegistro(int id)
+        {
+            var _autoDatos = autoDatos.FirstOrDefault(x => x.Id == id);
+
+            if (_autoDatos != null)
+            {
+                autoDatos.Remove(_autoDatos);
+            }
+
+            return RedirectToAction("Index");
+
+        }
+
+
+        [HttpPost]
+        public IActionResult Crear(AutoModel _autoDatos)
+        {
+
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    //Almacena los datos
+                    _autoDatos.Id = autoDatos.Count + 1;
+                    autoDatos.Add(_autoDatos);
+
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "No se pudo almacenar");
+            }
+
+            return View(_autoDatos);
+        }
+
+
+        [HttpPost]
+        public IActionResult Editar(AutoModel _autoDatos)
+        {
+            var _autoVerificarDatos = autoDatos.FirstOrDefault(x => x.Id == _autoDatos.Id);
+
+            if (_autoVerificarDatos == null)
+            {
+                return NotFound();
+            }
+
+            _autoVerificarDatos.Name = _autoDatos.Name;
+            _autoVerificarDatos.Description = _autoDatos.Description;
+            _autoVerificarDatos.IsActive = _autoDatos.IsActive;
+            _autoVerificarDatos.Marca = _autoDatos.Marca;
+
+            return RedirectToAction("Index");
+
+            
+        }
+
+        #endregion 
+
+
+
+
+    }
+}
---
+++ WebApplication3/WebApplication3/Models/AutoModel.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApplication3.Models
+{
+    public class AutoModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public string Marca { get; set; }
+
+    }
+}
