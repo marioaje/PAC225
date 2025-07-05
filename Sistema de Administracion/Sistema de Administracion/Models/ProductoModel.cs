1dba1b7559bbcc5e6e52df5c24ebb03ea788328b
marioaje <marioaje@gmail.com>
2025-07-04 19:08:43 -0600

    parte 4

--- Sistema de Administracion/Sistema de Administracion/Models/ProductoModel.cs
+++ Sistema de Administracion/Sistema de Administracion/Models/ProductoModel.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Descripcion { get; set; }        
-        public bool IsActive { get; set; }
+        //public bool IsActive { get; set; }
         public string Marca { get; set; }
     }
 }
--- Sistema de Administracion/Sistema de Administracion/Program.cs
+++ Sistema de Administracion/Sistema de Administracion/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Sistema_de_Administracion.Data;
@@ -7,13 +8,25 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseMySQL(builder.Configuration.GetConnectionString("ConnectionStrings"))
+    options.UseMySQL(builder.Configuration.GetConnectionString("MySqlConnection"))
 );
+
+//"ConnectionStrings": {
+//    "MySqlConnection": "server=srv863.hstgr.io;port=3306;database=u484426513_pac225;user=u484426513_pac225;password=rO46j4Qz/"
+//  },
 
 
 
 var app = builder.Build();
+
+app.MapControllerRoute(
+
+    name: "default",
+
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -25,6 +38,8 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+
 
 app.UseRouting();
 
